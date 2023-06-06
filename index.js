e5736d001f96c68926620a1cd952af8e9c1250dc
luageo <ldeoliveiratech@gmail.com>
2023-06-06 17:14:18 +0200

    rental offers and configurations created

--- index.js
+++ index.js
@@ -4,27 +4,70 @@ require("dotenv").config();
 const cors = require("cors");
 
 const app = express();
-app.use(cors);
+app.use(cors());
 app.use(express.json());
 
 const apiKey = process.env.API_KEY;
 
 app.get("/locations", async (req, res) => {
-  console.log("HELLOO");
   try {
     const response = await axios.get(
-      `https://lereacteur-bootcamp-api.herokuapp.com/api/sixt/locations${req.query.q}`,
+      `https://lereacteur-bootcamp-api.herokuapp.com/api/sixt/locations?q=${req.query.q}`,
       {
         headers: {
           Authorization: `Bearer ${apiKey}`,
         },
       }
     );
-    console.log(req.query.q);
     res.status(200).json(response.data);
   } catch (error) {
-    console.log("caiu no catch");
-    res.status(500).send({ message: error });
+    console.log(error);
+    res.status(500).json({ message: error });
+  }
+});
+
+app.get("/rentaloffers", async (req, res) => {
+  try {
+    const { pickupStation, returnStation, pickupDate, returnDate } = req.query;
+    if (returnDate < pickupDate) {
+      return res
+        .status(400)
+        .json({ error: "Return date cannot be earlier than pickup date" });
+    }
+    const response = await axios.get(
+      `https://lereacteur-bootcamp-api.herokuapp.com/api/sixt/rentaloffers?pickupStation=${pickupStation}&returnStation=${returnStation}&pickupDate=${pickupDate}&returnDate=${returnDate}`,
+      {
+        headers: {
+          Authorization: `Bearer ${apiKey}`,
+        },
+      }
+    );
+    res.status(200).json(response.data);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: error });
+  }
+});
+
+app.post("/rentalconfigurations/create", async (req, res) => {
+  try {
+    const { offerId } = req.body;
+    const payload = {
+      offerId,
+    };
+    const response = await axios.post(
+      "https://lereacteur-bootcamp-api.herokuapp.com/api/sixt/rentalconfigurations/create",
+      payload,
+      {
+        headers: {
+          Authorization: `Bearer ${apiKey}`,
+        },
+      }
+    );
+    res.status(200).json(response.data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: error });
   }
 });
 
